8bd7f9d0c5994e8e484bd65e3a7f36a3225b63d5
Connor [REDACTED] <blacksilverck35@gmail.com>
2018-01-15 00:48:33 +0000

    default index file

---
+++ basic_index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+	<head>
+		<title>FOR SCIENCE!</title>
+		
+<?php
+	include($_SERVER["DOCUMENT_ROOT"] . "/common/head.php");
+?>
+		<!-- TODO: OpenGraph -->
+	</head>
+	<body>
+<?php
+	include($_SERVER["DOCUMENT_ROOT"] . "/common/header.php");
+?>
+	
+	
+		
+	</body>
+</html>
