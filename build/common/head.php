135b3ebe88c8fd3e144b53ed5ea4c15cb35844a7
Connor [REDACTED] <blacksilverck35@gmail.com>
2018-01-15 00:52:52 +0000

    oops, been tracking the build folder.

--- build/common/head.php
+++
@@ -1,9 +0,0 @@
-<!--  from common/header.php -->
-		<link rel="stylesheet" type="text/css" href="/common/header.css" />
-		<script
-		  src="https://code.jquery.com/jquery-3.2.1.min.js"
-		  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-		  crossorigin="anonymous"></script>
-		<script src="/common/resources.js"> </script>
-		<script src="/common/common.js"> </script>
-
--- build/common/resources.js
+++
@@ -1,6 +0,0 @@
-var resources = {
-	science: 0,
-	money: 0,
-	science_diff: 1,
-	money_diff: 0
-}
